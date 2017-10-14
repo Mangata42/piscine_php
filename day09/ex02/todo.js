dd71270a0814632b82d354bb70f58f62dd7abd4b
Nader Ghr <Mangata@Curiosity.local>
2017-10-14 20:15:49 +0200

    day09

--- day09/ex02/todo.js
+++ day09/ex02/todo.js
@@ -1,6 +1,5 @@
 var todo_name;
-
-var todo_id = 0;
+var todo_id;
 
 function prompt_todo()
 {
@@ -18,8 +17,8 @@ function add_todo()
 	node.setAttribute("id", "list_" + todo_id);
 	var the_list = document.getElementById("ft_list");
 	the_list.insertBefore(node, the_list.childNodes[0]);
+	document.cookie = "list_" + todo_id + "=" + node.innerText;
 	todo_id++;
-	save_to_cookie();
 }
 
 function delete_todo(todo_id_to_del)
@@ -29,10 +28,40 @@ function delete_todo(todo_id_to_del)
 	{
 		var item = document.getElementById(todo_id_to_del);
 		item.parentNode.removeChild(item);
+		document.cookie = todo_id_to_del + "=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
 	}
 }
 
-function save_to_cookie()
+function load_cookies()
 {
-	
+	regpatt = new RegExp("-->[\\sa-zA-Z0-9_-]*<--$")
+	regpatt_2 = new RegExp("list_[0-9]+");
+	l_cookies = document.cookie;
+	if (l_cookies.length == 0)
+	{
+		todo_id = 0;
+		console.log("No Cookies!");
+	}
+	else
+	{
+		console.log("Cookies! Hurray!");
+		l_array = l_cookies.split(";");
+		todo_id = l_array.length;
+		id_backup = todo_id;
+		for (i = 0; i < l_array.length; i++)
+		{
+   			str = l_array[i];
+   			regres = regpatt.exec(str);
+   			regres = regres[0];
+   			list_index = regpatt_2.exec(str);
+   			list_index = list_index[0];
+   			var node_b = document.createElement("DIV");
+			node_b.appendChild(document.createTextNode(regres));
+			node_b.setAttribute("onclick", "delete_todo(this.id)");
+			node_b.setAttribute("id", list_index);
+			var the_list_b = document.getElementById("ft_list");
+			the_list_b.insertBefore(node_b, the_list_b.childNodes[0]);
+			console.log(node_b);
+		}
+	}
 }
